db7c6e76a39c6b30d7f9d4a1783dd403bd286bda
kosxatzi <kos.xatzi@gmail.com>
2016-03-13 21:36:59 +0000

    Update load_data.R

--- load_data.R
+++ load_data.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-loadData <- function() {
+mydata <- function() {
   read.table('data.txt', sep=';', header=TRUE) %>%
     mutate(Date = as.Date(Date, '%d/%m/%Y')) %>%
     mutate(Time = as.POSIXct(strptime(paste(Date, ' ', Time), '%Y-%m-%d %H:%M:%S'))) %>%
