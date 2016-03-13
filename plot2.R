e6b250dcb1e146503ca6f2e58fdf2d6969c1d8a9
kosxatzi <kos.xatzi@gmail.com>
2016-03-13 21:38:23 +0000

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,14 +1,14 @@
 
 source('load_data.R')
 
-data <- loadData()
+mydata <- loadData()
 
 png(filename='plot2.png', width=480, height=480, units='px')
 
 startDay = as.POSIXct(strftime("2007-02-01 00:00:00"))
 endDay = as.POSIXct(strftime("2007-02-03 00:00:00"))
-plot(data$Time,
-     data$Global_active_power,
+plot(mydata$Time,
+     mydata$Global_active_power,
      xlim=c(startDay, endDay),
      xaxt="n",
      type="l",
